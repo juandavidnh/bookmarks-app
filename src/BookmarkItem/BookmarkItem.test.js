bec138779a49c2be1847d5bc6d359f4297f8df85
Juan David Nunez <juandavidnh93@gmail.com>
2020-02-07 07:09:21 -0500

    deleted BookItems test

--- src/BookmarkItem/BookmarkItem.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BookmarkItem from './BookmarkItem';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BookmarkItem title="My test Bookmark" url="https://test.com" rating={4} description="Test bookmark's description to the max."/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
