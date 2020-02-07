8124d4da3a5829345e409b589bf0f7d808239516
Juan David Nunez <juandavidnh93@gmail.com>
2020-02-07 07:06:49 -0500

    fixed boomarkItem test

--- src/BookmarkItem/BookmarkItem.test.js
+++ src/BookmarkItem/BookmarkItem.test.js
@@ -4,6 +4,6 @@ import BookmarkItem from './BookmarkItem';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<BookmarkItem />, div);
+  ReactDOM.render(<BookmarkItem title="My test Bookmark" url="https://test.com" rating={4} description="Test bookmark's description to the max."/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
