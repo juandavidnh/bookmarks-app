3ce0912913f5b8a4f7e6422cc77bfb04ff886307
Juan David Nunez <juandavidnh93@gmail.com>
2020-02-07 07:17:48 -0500

    fixed api_endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: `https://protected-ocean-68347.herokuapp.com/api`,
+  API_ENDPOINT: `https://protected-ocean-68347.herokuapp.com/api/bookmarks`,
   API_KEY: process.env.REACT_APP_API_KEY,
 }
